1c2539ccf511319ecfb3dbcb206c5504da2089ef
mcy <mcy@163.com>
2023-12-14 13:50:13 +0800

    feature: 补充左侧缺少的logo;

--- site/src/components/appBar/index.js
+++ site/src/components/appBar/index.js
@@ -1,6 +1,8 @@
 import { Grid } from "@mui/material"
 import '../../css/btn.css'
 import StyledButton from "../commom/MyButton"
+import LogoIcon from '../../image/logo_v2.png'
+
 
 export default function AppBar() {
 
@@ -8,19 +10,24 @@ export default function AppBar() {
         <>
             <Grid container
                 sx={{
-                    justifyContent: 'flex-end',
                     my: 2
                 }}
             >
-                <Grid item
-                    sx={{
-                        mr: 2
-                    }}
-                >
-                    <StyledButton variant="text" className="btn">Pricing</StyledButton>
-                    <StyledButton variant="text" className="btn">Log in</StyledButton>
-                    <StyledButton variant="text" className="btn">Sign up</StyledButton>
-                    <StyledButton variant="contained" className="btn" >Create form</StyledButton>
+                <Grid item sm={6} xs={6} sx={{ pl: 2 }}>
+                    <img src={LogoIcon} alt='' width="70" height='28'></img>
+                </Grid>
+                <Grid item sm={6} xs={6}>
+                    <Grid container
+                        sx={{
+                            justifyContent: 'flex-end',
+                            pr:2
+                        }}>
+                        <StyledButton variant="text" className="btn">Pricing</StyledButton>
+                        <StyledButton variant="text" className="btn">Log in</StyledButton>
+                        <StyledButton variant="text" className="btn">Sign up</StyledButton>
+                        <StyledButton variant="contained" className="btn" >Create form</StyledButton>
+                    </Grid>
+
                 </Grid>
             </Grid>
         </>
