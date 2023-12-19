eacf57df0f45fa0e829932198f8d858c1085e5e6
mcy <mcy@163.com>
2023-12-19 17:18:43 +0800

    feature: 添加error page(3);

--- src/components/home/error.js
+++ src/components/home/error.js
@@ -1,16 +1,19 @@
-import {useRouteError} from 'react-router-dom'
+import { Container, Grid, Typography } from '@mui/material';
+import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+        <Container>
+            <Grid Container>
+                <Typography variant='h3'align='center'>Oops!</Typography>
+                <Typography variant="body1" align='center'>Sorry, an unexpected error has occurred.</Typography>
+                <Typography variant="body1" align='center'>
+                    <i>{error.statusText || error.message} 404</i>
+                </Typography>
+            </Grid>
+        </Container>
     );
 }
